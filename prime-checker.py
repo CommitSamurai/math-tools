b436ce1297d60b4adae79f214ce159a97f6d4542
CommitSamurai <166235608+CommitSamurai@users.noreply.github.com>
2026-05-27 15:24:08 -0300

    Update prime-checker.py

--- prime-checker.py
+++ prime-checker.py
@@ -1,3 +1,11 @@
 #checks to see if a number is prime
 def prime_checker(num: int):
-    pass
+    divisors = list()
+    for i in range(1, num + 1):
+        if (num % i == 0):
+            divisors.append(i)
+    if len(divisors) == 2:
+        return 'Prime'
+    else:
+        return 'Not prime'
+    
