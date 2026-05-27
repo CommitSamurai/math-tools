f879095b43060322d749b88b7a56904eabc06843
CommitSamurai <166235608+CommitSamurai@users.noreply.github.com>
2026-05-27 15:34:23 -0300

    Update bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,6 @@
+#bubble sort algorithm
+#steps through a list, compares adjacent elements
+#and swaps them if the first is bigger than the 
+#latter
+#example: unsorted list: [5, 1, 3, 4, 2]
+#bubble sorted list: [1, 2, 3, 4, 5]
