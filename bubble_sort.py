c40c9283cfe6cdb1463bba7d0b59129a4f177f57
CommitSamurai <166235608+CommitSamurai@users.noreply.github.com>
2026-05-27 15:41:28 -0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -6,10 +6,9 @@
 #bubble sorted list: [1, 2, 3, 4, 5]
 
 def bubble_sort(list: list):
-    for i, v in enumerate(list):
+    for i in range(len(list)):
         if list[i] > list[i+1]:
-            list[i+1].append(v)
-            list[i].append(v[i+1])
+            list.append(list[i])
     return list
 
 print(bubble_sort([5, 1, 3, 4, 2]))
