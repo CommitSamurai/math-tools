957124add64a60cf54e35ea029b0d7fa34576227
CommitSamurai <166235608+CommitSamurai@users.noreply.github.com>
2026-05-27 15:18:18 -0300

    Update greatest-common-divisor.py

--- greatest-common-divisor.py
+++ greatest-common-divisor.py
@@ -1,3 +1,8 @@
 def gcd(num1: int, num2: int):
-    
-    pass
+    #calculates the greatest common
+    #divisor between two numbers
+    gcdlist = list()
+    for i in range(1, min(num1, num2) + 1):
+        if i % num1 == 0 and i % num2 == 0:
+            gcdlist.append(i)
+    return max(gcdlist)
